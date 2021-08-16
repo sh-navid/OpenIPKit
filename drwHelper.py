ab19dff126e1e3aa11d60a942817bfc1e25f6607
sh-navid <shamshirsaz.navid@live.com>
2021-08-16 12:10:49 +0430

    Add marker and poly from opencv

--- drwHelper.py
+++ drwHelper.py
@@ -5,8 +5,8 @@
 #########################################################
 # Add preprocessors
 #########################################################
-import os
 import cv2
+import numpy as np
 import osHelper as osh
 
 #########################################################
@@ -19,6 +19,27 @@ DEFAULT_TICKNESS = 5
 # Functions
 #########################################################
 
+def drawMarker(im, pos, color=(128, 0, 255)):
+    return cv2.drawMarker(
+        im,
+        pos,
+        color=color,
+        markerType=cv2.MARKER_TRIANGLE_DOWN,
+        thickness=5,
+        markerSize=40,
+        line_type=cv2.LINE_AA,
+    )
+
+
+def drawPoly(im, points, color=(128, 0, 200), thickness=10):
+    return cv2.polylines(
+        im,
+        pts=[np.int32(points)],
+        isClosed=True,
+        color=color,
+        thickness=thickness,
+        lineType=cv2.LINE_AA,  # Anti-Aliased
+    )
 
 def drawLine(im, pt1, pt2, color=DEFAULT_COLOR, thickness=DEFAULT_TICKNESS, hasArrow=False):
     fn = cv2.line if not hasArrow else cv2.arrowedLine
@@ -49,7 +70,7 @@ if __name__ == '__main__':
     im = cv2.imread(dir+r'/media/im.png')
     h, w = im.shape[:2]
 
-    im = drawLine(im, (0, 0), (w//2, h//2))
+    im = drawLine(im, (0, 0), (w//2, h//2),hasArrow=True)
     
     cv2.imshow('Test',im)
     cv2.waitKey(0)
