133c6aad28e16d8424b493ae623fffa38e8f468f
sh-navid <shamshirsaz.navid@live.com>
2021-08-16 11:36:15 +0430

    Line and arrow

--- opencv/drwHelper.py
+++ opencv/drwHelper.py
@@ -8,16 +8,22 @@
 import os
 import cv2
 
-def line(im, start, end, color=(128, 0, 200), thickness=5):
-    return cv2.line(
+DEFAULT_COLOR = (128, 0, 200)
+DEFAULT_TICKNESS = 5
+
+
+def line(im, pt1, pt2, color=DEFAULT_COLOR, thickness=DEFAULT_TICKNESS, hasArrow=False):
+    fn = cv2.line if not hasArrow else cv2.arrowedLine
+    return fn(
         im,
-        pt1=(start[0], start[1]),
-        pt2=(end[0], end[1]),
+        pt1=pt1,
+        pt2=pt2,
         color=color,
         thickness=thickness,
-        lineType=cv2.LINE_AA,  # Anti-Aliased
+        lineType=cv2.LINE_AA  # Anti-Aliased
     )
 
-# https://en.wikipedia.org/wiki/B%C3%A9zier_curve
-def drawCurve(pt1,pt2,pt3,pt4):
+
+def drawCurve(pt1, pt2, pt3, pt4):
+    # https://en.wikipedia.org/wiki/B%C3%A9zier_curve
     pass
