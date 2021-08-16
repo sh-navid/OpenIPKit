97e5186eaf721528c63c50c8d455fd14940e84a7
sh-navid <shamshirsaz.navid@live.com>
2021-08-16 12:20:01 +0430

    Update draw methods

--- drwHelper.py
+++ drwHelper.py
@@ -14,37 +14,41 @@ import osHelper as osh
 #########################################################
 DEFAULT_COLOR = (128, 0, 200)
 DEFAULT_TICKNESS = 5
+DEFAULT_MARKER = cv2.MARKER_CROSS
+DEFAULT_MARKER_SIZE = DEFAULT_TICKNESS*2
 
 #########################################################
 # Functions
 #########################################################
 
-def drawMarker(im, pos, color=(128, 0, 255)):
-    return cv2.drawMarker(
+
+def drawMarker(im, pt, color=DEFAULT_COLOR, thickness=DEFAULT_TICKNESS, markerType=DEFAULT_MARKER, markerSize=DEFAULT_MARKER_SIZE):
+    cv2.drawMarker(
         im,
-        pos,
+        pt,
         color=color,
-        markerType=cv2.MARKER_TRIANGLE_DOWN,
-        thickness=5,
-        markerSize=40,
-        line_type=cv2.LINE_AA,
+        markerType=markerType,
+        thickness=int(thickness),
+        markerSize=markerSize,
+        line_type=cv2.LINE_AA
     )
 
 
-def drawPoly(im, points, color=(128, 0, 200), thickness=10):
-    return cv2.polylines(
+def drawPoly(im, pts, color=DEFAULT_COLOR, thickness=DEFAULT_TICKNESS):
+    cv2.polylines(
         im,
-        pts=[np.int32(points)],
+        pts=[np.int32(pts)],
         isClosed=True,
         color=color,
-        thickness=thickness,
-        lineType=cv2.LINE_AA,  # Anti-Aliased
+        thickness=int(thickness),
+        lineType=cv2.LINE_AA
     )
+
 
 def drawLine(im, pt1, pt2, color=DEFAULT_COLOR, thickness=DEFAULT_TICKNESS, hasArrow=False):
     fn = cv2.line if not hasArrow else cv2.arrowedLine
     type = 'lineType' if not hasArrow else 'line_type'
-    return eval(f'''fn(
+    eval(f'''fn(
         im,
         pt1=pt1,
         pt2=pt2,
@@ -70,8 +74,9 @@ if __name__ == '__main__':
     im = cv2.imread(dir+r'/media/im.png')
     h, w = im.shape[:2]
 
-    im = drawLine(im, (0, 0), (w//2, h//2),hasArrow=True)
-    
-    cv2.imshow('Test',im)
+    drawLine(im, (0, 0), (w//2, h//2), hasArrow=True)
+    drawPoly(im, (0, 0), (w, h//2), (w//2, h))
+
+    cv2.imshow('Test', im)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
