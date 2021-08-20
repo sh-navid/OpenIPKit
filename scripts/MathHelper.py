0a6a89fd994aad913cddc6c3c5d927a52bded9e1
sh-navid <shamshirsaz.navid@live.com>
2021-08-21 00:11:05 +0430

    Update

--- scripts/MathHelper.py
+++ scripts/MathHelper.py
@@ -18,8 +18,19 @@ def dist(pt1, pt2):
     return (abs(pt1[0]-pt2[0]), abs(pt1[1]-pt2[1]))
 
 
-def lineEq(pt1,pt2):
-    x1,y1=pt1[:2]
-    x2,y2=pt1[:2]
-    m=(y2-y1)/(x2-x1)
-    
+def lineEq(pt1, pt2):
+    x1, y1 = pt1[:2]
+    x2, y2 = pt2[:2]
+    m = (y2-y1)/(x2-x1)
+    # y=mx+b => b=y-mx
+    b = y1-m*x1
+    return m, b
+
+
+def calcLineX(m, b, y):
+    # y=mx+b => mx=y-b => x=(y-b)/m
+    return (y-b)/m
+
+
+def calcLineY(m, b, x):
+    return m*x+b
