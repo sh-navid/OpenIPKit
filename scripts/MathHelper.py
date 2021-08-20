fad9653ea6bcfeefefac2a80ee03ea2ec4ce931b
sh-navid <shamshirsaz.navid@live.com>
2021-08-21 00:07:27 +0430

    Update

--- scripts/MathHelper.py
+++ scripts/MathHelper.py
@@ -16,3 +16,10 @@ import math
 def dist(pt1, pt2):
     '''Return the distance between 2 points'''
     return (abs(pt1[0]-pt2[0]), abs(pt1[1]-pt2[1]))
+
+
+def lineEq(pt1,pt2):
+    x1,y1=pt1[:2]
+    x2,y2=pt1[:2]
+    m=(y2-y1)/(x2-x1)
+    
--- test_draw_curve.py
+++ test_draw_curve.py
@@ -9,6 +9,7 @@
 import cv2
 import scripts.drawHelper as drw
 import scripts.osHelper as osh
+import scripts.mathHelper as mth
 from scripts.colorHelper import *
 from inspect import signature
 
@@ -22,8 +23,15 @@ h, w = im.shape[:2]
 ox, oh, cx, cy, o = w/4, h/4, w/2, h/2, (w/2)/1.6
 rc = randomColor
 
-pt1,pt2,pt3=(ox,cy),(cx,oh),(w-ox,cy)
-drw.drawMultiLine(im,[pt1,pt2,pt3],thickness=1)
+pt1, pt2, pt3 = (ox, cy), (cx, oh), (w-ox, cy)
+drw.drawMultiLine(im, [pt1, pt2, pt3], thickness=1)
+
+slices=10
+_12=mth.dist(pt1,pt2)//slices
+_23=mth.dist(pt2,pt3)//slices
+
+for i in range(0,len(slices)):
+    cv2.circle(im,(pt1[0]),10,color=(120,30,240),thickness=3)
 
 cv2.imshow('Test', im)
 cv2.waitKey(0)
