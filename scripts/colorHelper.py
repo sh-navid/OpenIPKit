a32957ec77764ad29047f2beb4dfa55ea7c6f5bf
sh-navid <shamshirsaz.navid@live.com>
2021-08-22 11:08:18 +0430

    Change color

--- scripts/colorHelper.py
+++ scripts/colorHelper.py
@@ -7,6 +7,7 @@
 # Add preprocessors
 #########################################################
 import random
+import cv2
 
 #########################################################
 # Functions
@@ -23,3 +24,31 @@ def randomLightColors():
 
 def randomDarkColors():
     return randomColor(0, 127)
+
+
+def getHSV(im):
+    hsv = im.copy()
+    hsv = cv2.cvtColor(hsv, cv2.COLOR_BGR2HSV)
+    (h, s, v) = cv2.split(hsv)
+    return h,s,v
+
+def changeHue(im, hue=130):
+    h,s,v=getHSV(im)
+    h[:] = hue
+    hsv = cv2.merge([h, s, v])
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bgr
+
+def changeSaturation(im, sat=130):
+    h,s,v=getHSV(im)
+    s[:] = sat
+    hsv = cv2.merge([h, s, v])
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bgr
+
+def changeBrightness(im, val=130):
+    h,s,v=getHSV(im)
+    v[:] = val
+    hsv = cv2.merge([h, s, v])
+    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bgr
