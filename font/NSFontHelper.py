9d88d31d19e5ebb797bcd25c628171278bcf7b67
sh-navid <shamshirsaz.navid@live.com>
2021-08-23 23:19:57 +0430

    Update

--- font/NSFontHelper.py
+++ font/NSFontHelper.py
@@ -19,9 +19,16 @@ im = cv2.threshold(im, 225, 255, cv2.THRESH_BINARY)[1]
 im = ~im
 
 H, W = im.shape[:2]
+
+downscale = 25
+im = cv2.resize(im, (W//downscale, H//downscale))
+H, W = im.shape[:2]
+im = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY)[1]
+
+
 ROWS, COLS = 7, 6
-SW = W//COLS
-SH = int(SW*.915)
+SW = W/COLS
+SH = H/ROWS
 
 
 class Ch:
@@ -32,19 +39,26 @@ class Ch:
 
 
 elements = {
-    Ch(0,0,'A'),Ch(1,0,'B'),Ch(2,0,'C'),Ch(3,0,'D'),Ch(4,0,'E'),Ch(5,0,'F'),
-    Ch(0,1,'G'),Ch(1,1,'H'),Ch(2,1,'I'),Ch(3,1,'J'),Ch(4,1,'K'),Ch(5,1,'L'),
-    Ch(0,2,'M'),Ch(1,2,'N'),Ch(2,2,'O'),Ch(3,2,'P'),Ch(4,2,'Q'),Ch(5,2,'R'),
-    Ch(0,3,'S'),Ch(1,3,'T'),Ch(2,3,'U'),Ch(3,3,'V'),Ch(4,3,'W'),Ch(5,3,'X'),
-    Ch(0,4,'Y'),Ch(1,4,'Z'),Ch(2,4,'.'),Ch(3,4,':'),Ch(4,4,';'),Ch(5,4,','),
-    Ch(0,5,'0'),Ch(1,5,'1'),Ch(2,5,'2'),Ch(3,5,'3'),Ch(4,5,'4'),Ch(5,5,'5'),
-    Ch(0,6,'6'),Ch(1,6,'7'),Ch(2,6,'8'),Ch(3,6,'9'),Ch(4,6,'-'),Ch(5,6,'+'),
+    Ch(0, 0, 'A'), Ch(1, 0, 'B'), Ch(2, 0, 'C'), Ch(
+        3, 0, 'D'), Ch(4, 0, 'E'), Ch(5, 0, 'F'),
+    Ch(0, 1, 'G'), Ch(1, 1, 'H'), Ch(2, 1, 'I'), Ch(
+        3, 1, 'J'), Ch(4, 1, 'K'), Ch(5, 1, 'L'),
+    Ch(0, 2, 'M'), Ch(1, 2, 'N'), Ch(2, 2, 'O'), Ch(
+        3, 2, 'P'), Ch(4, 2, 'Q'), Ch(5, 2, 'R'),
+    Ch(0, 3, 'S'), Ch(1, 3, 'T'), Ch(2, 3, 'U'), Ch(
+        3, 3, 'V'), Ch(4, 3, 'W'), Ch(5, 3, 'X'),
+    Ch(0, 4, 'Y'), Ch(1, 4, 'Z'), Ch(2, 4, '.'), Ch(
+        3, 4, ':'), Ch(4, 4, ';'), Ch(5, 4, ','),
+    Ch(0, 5, '0'), Ch(1, 5, '1'), Ch(2, 5, '2'), Ch(
+        3, 5, '3'), Ch(4, 5, '4'), Ch(5, 5, '5'),
+    Ch(0, 6, '6'), Ch(1, 6, '7'), Ch(2, 6, '8'), Ch(
+        3, 6, '9'), Ch(4, 6, '-'), Ch(5, 6, '+'),
 }
 
 for yi in range(0, ROWS):
     for xi in range(0, COLS):
-        pt1 = (xi*SW, yi*SH)
-        pt2 = (xi*SW+SW, yi*SH+SH)
+        pt1 = (int(xi*SW), int(yi*SH))
+        pt2 = (int(xi*SW+SW), int(yi*SH+SH))
         cv2.rectangle(im, pt1, pt2, (0, 255, 0), 1)
 
 cv2.imwrite(sys.path[0]+'/NSFreeFontProcessed.png', im)
