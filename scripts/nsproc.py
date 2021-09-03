5cdc6f5ed8a7e4b2d443b5237b9f2f1b0752deb3
sh-navid <shamshirsaz.navid@live.com>
2021-09-03 17:00:15 +0430

    Update

--- scripts/nsproc.py
+++ scripts/nsproc.py
@@ -5,6 +5,12 @@ def fill(im, pos=(0, 0), color=(0, 0, 0), diff=0):
     pass
 
 
+def tresh_channel(im,thresh,channel=0):
+    im[np.where(im[:,:,channel] < thresh)] = 0
+    im[np.where(im[:,:,channel] >= thresh)] = 255
+    return im
+
+
 def tresh_simple(im, thresh):
     im[np.where(im < thresh)] = 0
     im[np.where(im >= thresh)] = 255
