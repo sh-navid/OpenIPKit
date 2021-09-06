7902963d0e9109cfc85268e16346c416a4af753b
sh-navid <shamshirsaz.navid@live.com>
2021-09-06 23:37:01 +0430

    I wrote a simple model generator for machine learning

--- scripts/ml/nslearn.py
+++ scripts/ml/nslearn.py
@@ -12,32 +12,37 @@ import scripts.backend as back
 I want to make a very siple machine learning algorithm from scratch
 '''
 
-BLOCK_SIZE=4
-def simpleCategoryLearn(imList,size,catNo):
+BLOCK_SIZE = 4
+
+
+def simpleCategoryLearn(imList, size, catNo):
     '''
     imList: [im1, im2, ..., imN]
     size: 8, 16, 32, 64
     '''
-    off=size//BLOCK_SIZE
-    samples=[]
+    off = size//BLOCK_SIZE
+    samples = []
     for im in imList:
-        sample=[]
+        im = back.resize(im, (size, size))
+        sample = []
         for r in off:
             for c in off:
+                y, x = r*BLOCK_SIZE, c*BLOCK_SIZE
+                block = im[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE]
+                sample.append(np.median(block))
                 pass
         samples.append(sample)
+    return {'size': size, 'catNo': catNo, 'samples': samples}
 
-    '''...'''
-    return {'size':size,'catNo':catNo,'samples':samples}
 
-def simpleCategoryDetect(im,model,checkMirror=False,checkUpsideDown=False,checkRotated=False):
+def simpleCategoryDetect(im, model, checkMirror=False, checkUpsideDown=False, checkRotated=False):
     '''
     model: {'size':size,'catNo':catNo,'samples':samples}
     '''
-    size=model['size']
-    catNo=model['catNo']
-    samples=model['samples']
-    off=size//BLOCK_SIZE
-    bestProbability=0
+    size = model['size']
+    catNo = model['catNo']
+    samples = model['samples']
+    off = size//BLOCK_SIZE
+    bestProbability = 0
     '''...'''
     return bestProbability
