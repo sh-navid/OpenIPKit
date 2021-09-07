642ac79dbd58309be29abf712c1b3ac83b1e668f
sh-navid <shamshirsaz.navid@live.com>
2021-09-07 16:16:43 +0430

    Removed test-draw-curve.py and moved its content to nsdraw

--- tests/test-draw-curve-second.py
+++ tests/test-draw-curve-second.py
@@ -24,7 +24,7 @@ im = back.imread(sys.path[0]+'/media/im.png')
 
 def run():
     h, w = im.shape[:2]
-    ox, oh, cx, cy, o = w/4, h/4, w/2, h/2, (w/2)/1.6
+    ox, oh, cx = w/4, h/4, w/2
     rc = nsc.randomColor
 
     pt1, pt2, pt3 = (ox, h-h/8), (cx, oh), (w-ox, h-h/8)
